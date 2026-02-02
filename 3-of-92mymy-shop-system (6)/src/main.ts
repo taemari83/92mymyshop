9da6b451d4976fc89ed9c3dd19927adb80c6ab44
taemari83 <taemari83@gmail.com>
2026-02-02 17:14:42 +0800

    Update main.ts

--- 3-of-92mymy-shop-system (6)/src/main.ts
+++ 3-of-92mymy-shop-system (6)/src/main.ts
@@ -1,9 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { provideZonelessChangeDetection } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { provideZoneChangeDetection } from '@angular/core'; // 改這裡
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideZonelessChangeDetection()
+    provideZoneChangeDetection({ eventCoalescing: true }) // 改這裡
   ]
 }).catch(err => console.error(err));
