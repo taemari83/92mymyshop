6632096c960f9d1468751efd82802bf2857ffd38
taemari83 <taemari83@gmail.com>
2026-02-02 17:42:31 +0800

    Update main.ts

--- 3-of-92mymy-shop-system (6)/src/main.ts
+++ 3-of-92mymy-shop-system (6)/src/main.ts
@@ -1,3 +1,4 @@
+import 'zone.js';  // <--- 加入這一行 (非常重要！)
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { provideZoneChangeDetection } from '@angular/core';
